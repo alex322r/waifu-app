f1894f1ce3d32470028e8fa04c20a4825ec25855
alexis <alexis@alexis>
2023-01-08 02:48:06 -0500

    waifu-app 1.0.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import iconSrc from './assets/iconsrc.png'
+import refresh from './assets/refresh.png'
 
 function App() {
+
+  const apiUrl = 'https://api.waifu.im/search';
+    const [pic, setPic] = useState('');
+    const [bkcolor, setBkcolor] = useState('');
+    const [source, setSource] = useState('');
+
+    const getWaifu = function(){
+      return fetch(apiUrl)
+      .then(res => res.json())
+      .then(response =>{
+        const {images} = response
+        const pic =images[0]['url']
+        const bkcolor = images[0]['dominant_color']
+        const source = images[0]['source']
+        setPic(pic)
+        setBkcolor(bkcolor)
+        setSource(source)
+      })
+    }
+    useEffect(function(){
+      getWaifu();
+    },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section style={{
+        backgroundColor : bkcolor
+      }}>
+            <a href={source} rel="noreferrer" target='_blank'><img alt='link-icon' className='waifu-icon' src={iconSrc}/></a>
+            <img className='waifu-pic' src={pic} alt='anime-waifu'/>
+            <button onClick={()=>getWaifu()}><img alt='refresh-icon' className='refresh' src={refresh}/></button>
+        </section>
     </div>
   );
 }
